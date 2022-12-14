2adf8be40427574af8ecb7d813f1c833fd97ea5d
ZZBase78 <88794134+ZZBase78@users.noreply.github.com>
2022-12-14 22:45:26 +0300

    Убрана переменная имени игрока, т.к. будем делать отдельное поле для ввода

--- Assets/_Root/Scripts/SolarSystemNetworkManager.cs
+++ Assets/_Root/Scripts/SolarSystemNetworkManager.cs
@@ -6,12 +6,12 @@ namespace Main
 {
     public class SolarSystemNetworkManager : NetworkManager
     {
-        [SerializeField] private string _playerName;
+        //[SerializeField] private string _playerName;
         public override void OnServerAddPlayer(NetworkConnectionToClient conn)
         {
             var spawnTransform = GetStartPosition();
             var player = Instantiate(playerPrefab, spawnTransform.position, spawnTransform.rotation);
-            player.GetComponent<ShipController>().PlayerName = _playerName;
+            //player.GetComponent<ShipController>().PlayerName = _playerName;
             player.name = $"{playerPrefab.name} [connId={conn.connectionId}]";
             NetworkServer.AddPlayerForConnection(conn, player);
         }
